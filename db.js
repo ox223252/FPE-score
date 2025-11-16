e175c03a53802fde9276417b0e6fa740e1285b8f
ox223252 <23252@laposte.net>
2025-11-16 09:48:25 +0100

    use JSON5 to allow comments
    Modifications qui seront validées :
    	modifié :         db.js

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 import fs from 'fs'; // read / write files
+import JSON5 from "json5"; // json with comment
 
 export function dbInit ( params )
 {
@@ -10,8 +11,9 @@ export function dbInit ( params )
 		}
 
 		let data =fs.readFileSync ( params.db.path[ key ], "utf8" );
+
 	 	try {
-			params.db[ key ] = JSON.parse ( data );
+			params.db[ key ] = JSON5.parse ( data );
 		}
 		catch ( e )
 		{
